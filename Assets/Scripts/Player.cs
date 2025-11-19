ae3bd27c79b85f3348e56735c4cee726a3a18624
Wajeeha Noor <112068947+wnz24@users.noreply.github.com>
2025-11-19 18:01:18 +0500

    Add Character Controls

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 7f;
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1;
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+
+        }
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y) * Time.deltaTime * moveSpeed;
+        transform.position += moveDir;
+
+        float rotationSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward,moveDir,Time.deltaTime * rotationSpeed);
+        Debug.Log($"Input Vector: {inputVector}");
+
+    }
+}
