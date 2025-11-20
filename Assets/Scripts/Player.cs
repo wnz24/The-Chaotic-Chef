17db2995a07738a26712574fa4be0629a7ff4f88
Wajeeha Noor <112068947+wnz24@users.noreply.github.com>
2025-11-20 09:39:31 +0500

    Add Animation to Player and cinemachine

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -31,9 +32,16 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y) * Time.deltaTime * moveSpeed;
         transform.position += moveDir;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward,moveDir,Time.deltaTime * rotationSpeed);
         Debug.Log($"Input Vector: {inputVector}");
 
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+
+    private const string IS_WALKING = "IsWalking";
+    [SerializeField] private Player player;
+    private Animator animator;
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool(IS_WALKING, player.IsWalking());
+    }
+}
+
