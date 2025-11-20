1c808be12f93f936690b3693ce0900e54dd54d3b
Wajeeha Noor <112068947+wnz24@users.noreply.github.com>
2025-11-20 16:58:43 +0500

    Add Player Collision Detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,56 @@ public class Player : MonoBehaviour
     private bool isWalking;
     private void Update()
     {
-       Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
+        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y) * Time.deltaTime * moveSpeed;
-        transform.position += moveDir;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(
+            transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance
+            );
+
+        if (!canMove)
+        {
+            //Cannot move towards moveDir
+
+            //Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(
+            transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance
+            );
+
+            if(canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                //Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(
+                transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance
+                );
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+
+        }
 
         isWalking = moveDir != Vector3.zero;
 
         float rotationSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward,moveDir,Time.deltaTime * rotationSpeed);
-        Debug.Log($"Input Vector: {inputVector}");
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+        //Debug.Log($"Input Vector: {inputVector}");
 
     }
 
